9324a1ad78cc933b3b92dc5186afe4b0bc0718bf
XiaoYuanJW <2754742370@qq.com>
2022-02-02 17:52:39 +0800

    Default Changelist

--- src/main/java/com/example/mall/controller/admin/CategoryController.java
+++ src/main/java/com/example/mall/controller/admin/CategoryController.java
@@ -1,6 +1,9 @@
 package com.example.mall.controller.admin;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.mall.controller.BaseController;
 import com.example.mall.entity.Category;
 import com.example.mall.service.AdminService;
@@ -8,25 +11,28 @@ import com.example.mall.service.CategoryService;
 import com.example.mall.service.LastIDService;
 import com.example.mall.util.PageUtil;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.List;
 import java.util.Map;
-
-import static jdk.nashorn.internal.objects.NativeDebug.map;
+import java.util.UUID;
 
 /**
  * 后台管理-产品类型页
  */
 @Controller
 public class CategoryController extends BaseController {
-    @Resource(name = "adminService")
-    private AdminService adminService;
     @Resource(name = "categoryService")
     private CategoryService categoryService;
+    @Resource(name = "lastIDService")
+    private LastIDService lastIDService;
 
     /*
     转到后台管理——产品类型页
@@ -34,7 +40,7 @@ public class CategoryController extends BaseController {
     @RequestMapping(value = "admin/category", method = RequestMethod.GET)
     public String goToPage(HttpSession session, Map<String,Object> map){
         logger.info("获取前10条产品类型列表");
-        PageUtil pageUtil = new PageUtil(0,5);
+        PageUtil pageUtil = new PageUtil(0,10);
         List<Category> categoryList = categoryService.getList(null, pageUtil);
         map.put("categoryList",categoryList);
         logger.info("获取产品类型总数量");
@@ -47,4 +53,135 @@ public class CategoryController extends BaseController {
         return "admin/categoryManagePage";
     }
 
+    /*
+    转到后台管理-产品类型详情页
+     */
+    @RequestMapping(value = "admin/category/{cid}", method = RequestMethod.GET)
+    public String goToDetailsPage(HttpSession session, Map<String,Object> map, @PathVariable Integer cid){
+        logger.info("获取category_id为{}的分类信息", cid);
+        Category category = categoryService.get(cid);
+        map.put("category", category);
+
+        logger.info("转到后台管理-分类详情页-ajax方式");
+        return "admin/include/categoryDetails";
+    }
+
+    /*
+    按条件查询产品类型
+     */
+    @ResponseBody
+    @RequestMapping(value = "admin/category/{index}/{count}",method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    public String getCategoryBySearch(@RequestParam(required = false)String category_name,
+                                      @PathVariable Integer index,
+                                      @PathVariable Integer count) throws UnsupportedEncodingException{
+        if(category_name != null){
+            //如果为非空字符串-->解决中文乱码：URLDecoder.decode(String,"UTF-8");
+            category_name = "".equals(category_name) ? null : URLDecoder.decode(category_name, "UTF-8");
+        }
+        JSONObject object = new JSONObject();
+        logger.info("按条件获取第{}页的{}条分类", index + 1 , count);
+        PageUtil pageUtil = new PageUtil(index,count);
+        List<Category> categoryList = categoryService.getList(category_name, pageUtil);
+        object.put("categoryList", JSONArray.parseArray(JSON.toJSONString(categoryList)));
+        logger.info("按条件获取分类总数量");
+        Integer categoryCount = categoryService.getTotal(category_name);
+        object.put("categoryCount",categoryCount);
+        logger.info("获取分页信息");
+        pageUtil.setTotal(categoryCount);
+        object.put("totalPage", pageUtil.getTotalPage());
+        object.put("pageUtil", pageUtil);
+        return object.toJSONString();
+    }
+
+    /*
+    转到后台管理-产品类型添加页
+     */
+    @RequestMapping(value = "admin/category/new",method = RequestMethod.GET)
+    public String goToAddPage(HttpSession session,Map<String,Object> map){
+        logger.info("转到后台管理-分类添加页");
+        return "admin/include/categoryDetails";
+    }
+
+    /*
+    添加产品类型信息
+     */
+    @ResponseBody
+    @RequestMapping(value = "admin/category",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    public String addCategory(@RequestParam String category_name,
+                              @RequestParam String category_image_src){
+        JSONObject jsonObject = new JSONObject();
+        logger.info("整合分类信息");
+        Category category = new Category()
+                .setCategory_name(category_name)
+                .setCategory_image_src(category_image_src.substring(category_image_src.lastIndexOf("/") + 1));
+        logger.info("添加分类信息");
+        boolean yn = categoryService.add(category);
+        if(yn){
+            int category_id = lastIDService.selectLastID();
+            logger.info("添加成功！新增分类的ID的值为：{}",category_id);
+            jsonObject.put("success", true);
+            jsonObject.put("category_id",category_id);
+        }else{
+            jsonObject.put("success", false);
+            logger.warn("添加失败！事务回滚");
+            throw new RuntimeException();
+        }
+        return jsonObject.toJSONString();
+    }
+
+    /*
+    更新产品类型信息
+     */
+    @ResponseBody
+    @RequestMapping(value = "admin/category/{category_id}",method = RequestMethod.PUT,produces = "application/json;charset=utf-8")
+    public String updateCategory(@RequestParam String category_name,
+                                 @RequestParam String category_image_src,
+                                 @PathVariable("category_id") Integer category_id){
+        JSONObject jsonObject = new JSONObject();
+        logger.info("整合更新分类信息");
+        Category category = new Category()
+                .setCategory_id(category_id)
+                .setCategory_name(category_name)
+                .setCategory_image_src(category_image_src.substring(category_image_src.lastIndexOf("/") + 1));
+        logger.info("更新分类信息，分类ID值为：{}", category_id);
+        boolean yn = categoryService.update(category);
+        if(yn){
+            logger.info("更新成功");
+            jsonObject.put("success", true);
+            jsonObject.put("category_id", category_id);
+        }else{
+            jsonObject.put("success", false);
+            logger.info("更新失败！事务回滚");
+            throw new RuntimeException();
+        }
+        return jsonObject.toJSONString();
+    }
+
+    /*
+    上传产品类型图片
+     */
+    @ResponseBody
+    @RequestMapping(value = "admin/uploadCategoryImage",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    public String uploadCategoryImage(@RequestParam MultipartFile file, HttpSession session) {
+        String originalFileName = file.getOriginalFilename();
+        logger.info("获取图片原始文件名:  {}", originalFileName);
+        String extension = originalFileName.substring(originalFileName.lastIndexOf('.'));
+        String fileName = UUID.randomUUID() + extension;
+        String filePath = session.getServletContext().getRealPath("/") + "res/images/item/categoryPicture/" + fileName;
+
+        logger.info("文件上传路径：{}", filePath);
+        JSONObject object = new JSONObject();
+        try {
+            logger.info("文件上传中...");
+            file.transferTo(new File(filePath));
+            logger.info("文件上传完成");
+            object.put("success", true);
+            object.put("fileName", fileName);
+        } catch (IOException e) {
+            logger.warn("文件上传失败!");
+            e.printStackTrace();
+            object.put("success", false);
+        }
+        return object.toJSONString();
+    }
 }
